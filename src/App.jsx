0eaf4d4fb152830f2ac257e6c4541e116ea5ed45
Arun <arunkk1872002@gmail.com>
2025-09-14 13:29:58 +0530

    feat: setup TailwindCSS, add homepage & NotFound page, install dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
 
-function App() {
+const App = () => {
   return (
     <>
+      <Routes>
+        <Route index element={<Home/>}>
 
+        </Route>
+      </Routes>
     </>
   )
 }
