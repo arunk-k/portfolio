b0009bfbed525446228fee653d3ec15248aa8d8e
Arun <arunkk1872002@gmail.com>
2025-09-16 10:39:02 +0530

    feat: add StarBackground effect and cn utility for class management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,15 @@
+import StarBackground from "../components/StarBackground"
+import ThemeToggle from "../components/ThemeToggle"
 
 const Home = () => {
   return (
     <>
-      
+      <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Theme Toggle  */}
+        <ThemeToggle />
+        {/* Background Effects  */}
+        <StarBackground />
+      </div>
     </>
   )
 }
