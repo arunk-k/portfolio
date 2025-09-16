967c6a12b101cca3bebe518337d9c44da3454df5
Arun <arunkk1872002@gmail.com>
2025-09-17 01:23:06 +0530

    feat: complete portfolio layout with ContactSection, Footer, and global Toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
+import { Toaster } from "./components/ui/toaster"
 
 const App = () => {
   return (
     <>
+    <Toaster/>
       <Routes>
         <Route index element={<Home />}></Route>
         <Route path="*" element={<NotFound />}></Route>
